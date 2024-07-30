4fe28fc60aa1682c543e065c080e69279c7021cc
Francisco Torre <161626436+TorreFrancisco@users.noreply.github.com>
2024-07-30 17:40:28 -0300

    Update side_menu.dart

--- lib/features/shared/widgets/side_menu.dart
+++ lib/features/shared/widgets/side_menu.dart
@@ -51,7 +51,7 @@ class SideMenuState extends ConsumerState<SideMenu> {
 
         Padding(
           padding: const EdgeInsets.fromLTRB(20, 0, 16, 10),
-          child: Text('Tony Stark', style: textStyles.titleSmall ),
+          child: Text('We are at products page!', style: textStyles.titleSmall ),
         ),
 
         const NavigationDrawerDestination(
